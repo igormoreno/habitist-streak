557cf5ea4dea3955e5d596908857c8ddead5b20b
Igor Moreno <igormoreno@gmail.com>
2019-05-04 12:56:10 +0200

    fix logging issue

--- habits.py
+++ habits.py
@@ -7,11 +7,13 @@ from datetime import datetime, timedelta
 import os
 import re
 import logging
+import sys
 from todoist.api import TodoistAPI
 import requests
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
 
 label_to_track = 'track'
 
@@ -22,12 +24,12 @@ def get_existio_token():
     return os.getenv('EXISTIO_APIKEY')
 
 def tag_existio(tag):
+    logger.info('tagging in exist.io: %s',tag)
     response = requests.post(
                       'https://exist.io/api/1/attributes/custom/append/',
                       headers={'Authorization':'Bearer '+get_existio_token()},
                       json={"value":tag})
-    logging.info('tagging in existio: %s',tag)
-    logging.info('response: %s',response.text)
+    logger.info('response: %s',response.text)
     return response
 
 
@@ -75,7 +77,7 @@ def track_label_id(api):
     return None
 
 def track_task(api, task_url):
-    logging.info("request to track task: %s",task_url)
+    logger.info("request to track task: %s",task_url)
     tasks = api.state['items']
     for task in tasks:
         if is_in_url(task, task_url) and track_label_id(api) in task['labels']:
@@ -94,7 +96,7 @@ def main():
     TODOIST_API_TOKEN = get_todoist_token()
     EXISTIO_API_TOKEN = get_existio_token()
     if not TODOIST_API_TOKEN or not EXISTIO_API_TOKEN:
-        logging.warning('Please set the API token in environment variable.')
+        logger.warning('Please set the API token in environment variable.')
         exit()
     api = TodoistAPI(TODOIST_API_TOKEN)
     api.sync()
